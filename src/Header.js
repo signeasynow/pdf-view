68cff16c853b9330d3efb5b08691ff9142d7b4fc
Alexander Leon <alexanderleon@Alexanders-MacBook-Pro.local>
2023-06-25 11:37:41 -0400

    w

--- src/Header.js
+++ src/Header.js
@@ -6,23 +6,55 @@ import 'pdfjs-dist/web/pdf_viewer.css';
 import Hand from '../assets/hand.svg';
 // import Pan from "./assets/pan.svg";
 
+
+const tooltip = css`
+  position: relative;
+  display: inline-block;
+  border-bottom: 1px dotted black;
+
+  &:hover::after {
+    content: attr(title);
+    position: absolute;
+    z-index: 1;
+    top: 120%;
+    left: 50%;
+    transform: translateX(-50%);
+    padding: 5px;
+    color: #fff;
+    background-color: #333;
+    border-radius: 3px;
+    white-space: nowrap;
+  }
+`;
+
+const Tooltip = ({ children, title }) => (
+  <div css={tooltip} title={title}>
+    {children}
+  </div>
+);
+
 const HeaderIcon = ({ src, alt }) => (
-  <img css={css({ width: 24, height: 24, cursor: "pointer"})} src={src} alt={alt} />
+  <img css={css({ width: 24, height: 24, cursor: "pointer"})} src={src} alt="" />
 )
 
 const Header = () => {
 
   return (
     <div id="controls">
-      <button>Menu</button>
+      <Tooltip title="Hello there">
+        <button>Menu</button>
+      </Tooltip>
+      
       |
       <button>Panel</button>
       <button>View controls</button>
       |
       <button id="zoomIn">Zoom In</button>
       <button id="zoomOut">Zoom Out</button>
-      <button>Pan</button>
-      <HeaderIcon src={Hand} alt="Pan" />
+      <Tooltip title="Pan">
+        <HeaderIcon src={Hand} alt="Pan" />
+      </Tooltip>
+      
 
       <select>
         <option>View</option>
