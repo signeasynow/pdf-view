eb773d291fcc217e9cce85371add084a4de694fb
Alexander Leon <alexanderleon@Alexanders-MacBook-Pro.local>
2023-06-24 22:31:07 -0400

    wip

---
+++ App.js
@@ -0,0 +1,68 @@
+import { h, Component } from 'preact';
+import { useEffect, useRef, useState } from 'preact/hooks';
+import * as pdfjs from 'pdfjs-dist';
+import { EventBus, PDFLinkService, PDFViewer, PDFFindController, PDFScriptingManager } from "pdfjs-dist/web/pdf_viewer";
+import 'pdfjs-dist/web/pdf_viewer.css';
+
+const SANDBOX_BUNDLE_SRC = "pdfjs-dist/build/pdf.sandbox.js";
+
+const PdfViewer = ({ file }) => {
+  const viewerContainerRef = useRef(null);
+
+  useEffect(() => {
+    if (!file || !viewerContainerRef.current) return;
+
+    const viewerContainer = viewerContainerRef.current;
+
+    const eventBus = new EventBus();
+    const pdfLinkService = new PDFLinkService({ eventBus, externalLinkTarget: 2 });
+    const pdfFindController = new PDFFindController({ eventBus, linkService: pdfLinkService });
+    const pdfScriptingManager = new PDFScriptingManager({ eventBus, sandboxBundleSrc: SANDBOX_BUNDLE_SRC });
+    const pdfViewer = new PDFViewer({ container: viewerContainer, eventBus, linkService: pdfLinkService, findController: pdfFindController, scriptingManager: pdfScriptingManager });
+
+    pdfLinkService.setViewer(pdfViewer);
+    pdfScriptingManager.setViewer(pdfViewer);
+
+    eventBus.on("pagesinit", function () {
+      pdfViewer.currentScaleValue = "page-width";
+    });
+
+    const loadingTask = pdfjs.getDocument(file);
+
+    loadingTask.promise.then(
+      loadedPdfDocument => {
+        pdfViewer.setDocument(loadedPdfDocument);
+        pdfLinkService.setDocument(loadedPdfDocument, null);
+      },
+      reason => {
+        console.error(reason);
+      }
+    );
+  }, [file]);
+
+  return <div ref={viewerContainerRef} id="viewer" class="pdfViewer"></div>;
+};
+
+const App = () => {
+  const [file, setFile] = useState(null);
+
+  useEffect(() => {
+    window.addEventListener('message', function(event) {
+      if (typeof event.data === 'object' && event.data.file) {
+        setFile(event.data.file);
+      }
+    }, false);
+  }, []);
+
+  return (
+    <div>
+      <div id="controls">
+        <button id="zoomIn">Zoom In</button>
+        <button id="zoomOut">Zoom Out</button>
+      </div>
+      <PdfViewer file={file} />
+    </div>
+  );
+};
+
+export default App;
--- loader.js
+++ loader.js
@@ -3,49 +3,8 @@ import * as pdfjs from 'pdfjs-dist';
 import { EventBus, PDFLinkService, PDFViewer, PDFFindController, PDFScriptingManager } from "pdfjs-dist/web/pdf_viewer";
 import 'pdfjs-dist/web/pdf_viewer.css';
 
-const SANDBOX_BUNDLE_SRC = "pdfjs-dist/build/pdf.sandbox.js";
-
+import { h, render } from 'preact';
+import App from './App';
 pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js`
 
-// Function to load PDF 
-function loadPdf(file) {
-  // Select the container in the iframe
-  const viewerContainer = document.getElementById('viewerContainer');
-  const eventBus = new EventBus();
-
-  // PDFJS related setup
-  const pdfLinkService = new PDFLinkService({ eventBus, externalLinkTarget: 2 });
-  const pdfFindController = new PDFFindController({ eventBus, linkService: pdfLinkService });
-  const pdfScriptingManager = new PDFScriptingManager({ eventBus, sandboxBundleSrc: SANDBOX_BUNDLE_SRC });
-  const pdfViewer = new PDFViewer({ container: viewerContainer, eventBus, linkService: pdfLinkService, findController: pdfFindController, scriptingManager: pdfScriptingManager });
-  
-  pdfLinkService.setViewer(pdfViewer);
-  pdfScriptingManager.setViewer(pdfViewer);
-
-  eventBus.on("pagesinit", function () {
-    pdfViewer.currentScaleValue = "page-width";
-  });
-
-  const loadingTask = pdfjs.getDocument(file);
-
-  loadingTask.promise.then(
-    loadedPdfDocument => {
-      pdfViewer.setDocument(loadedPdfDocument);
-      pdfLinkService.setDocument(loadedPdfDocument, null);
-    },
-    reason => {
-      console.error(reason);
-    }
-  );
-}
-
-// Call function to load PDF
-window.addEventListener('message', function(event) {
-  // Check the origin of the data!
-  if (typeof event.data === 'object' && event.data.file) {
-      const file = event.data.file;
-      console.log(file ,'file! doggg')
-      loadPdf(file);
-      // Initialize your PDF viewer here with file
-  }
-}, false);
+render(<App />, document.getElementById('app'));
