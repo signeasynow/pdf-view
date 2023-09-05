6edd2751970d6ef83e85b2232a877c686bf9e8be
Alexander Leon <alexanderleon@Alexanders-MacBook-Pro.local>
2023-09-04 20:26:33 -0400

    w

--- src/components/Panel/index.js
+++ src/components/Panel/index.js
@@ -1,7 +1,5 @@
 /** @jsxImportSource @emotion/react */
 import { css } from '@emotion/react';
-
-import 'pdfjs-dist/web/pdf_viewer.css';
 // import Pan from "./assets/pan.svg";
 import { useState } from 'preact/hooks';
 import ThumbnailsSection from './ThumbnailsSection';
@@ -30,6 +28,16 @@ const optionsWrapper = css`
 
 `;
 
+const thumbnailTopActionsWrapper = css`
+  background: #d6dee8;
+	width: 100px;
+	display: flex;
+	align-items: center;
+	justify-content: center;
+	margin-top: 8px;
+	border-radius: 4px;
+`
+
 const Panel = ({
 	showPanel,
 	tools,
@@ -46,9 +54,11 @@ const Panel = ({
 	return (
 		<div css={showPanel ? visibleSearchWrapper : invisibleSearchWrapper}>
 				<div css={optionsWrapper}>
-					<Tooltip title={t("view thumbnails in full screen")}>
-						<Icon onClick={() => setActiveTab(0)} src={ExpandIcon} alt={t("expand")} />
-					</Tooltip>
+					<div css={thumbnailTopActionsWrapper}>
+						<Tooltip title={t("view thumbnails in full screen")}>
+							<Icon onClick={() => setActiveTab(0)} src={ExpandIcon} alt={t("expand")} />
+						</Tooltip>
+					</div>
 					{
 						/*
 						<Tooltip title="Bookmarks">
