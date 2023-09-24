4b27395aa433ab59084ec65dcce508e4935fdc6f
Alexander Leon <alexanderleon@Alexanders-MacBook-Pro.local>
2023-09-24 09:38:36 -0400

    w

--- src/utils/fetchBuffers.js
+++ src/utils/fetchBuffers.js
@@ -1,4 +1,19 @@
-export async function fetchBuffers(files, retrievePDF, blobUrlToArrayBuffer) {
+import { retrievePDF } from "./indexDbUtils";
+import * as pdfjs from 'pdfjs-dist';
+
+const blobUrlToArrayBuffer = async (blobUrl) => {
+  try {
+    const loadingTask = pdfjs.getDocument(blobUrl);
+    const pdfDoc = await loadingTask.promise;
+    return await pdfDoc.getData();
+    // return new Uint8Array(pdfData); // or pdfData.buffer if Uint8Array is not suitable
+  } catch (err) {
+    console.error(`Error while reading PDF: ${err.message}`);
+    throw err; // or return some default/fallback value
+  }
+};
+
+async function fetchBuffers(files) {
   // Initialize tasks to retrieve PDFs
   const arr = Array.from({ length: files.length }).fill(null);
   const tasks = arr.map((_, idx) => retrievePDF(`pdfId${idx}`));
@@ -28,5 +43,5 @@ export async function fetchBuffers(files, retrievePDF, blobUrlToArrayBuffer) {
   return successfulBuffers; // This will be an array of all the successful buffers.
 }
 
-
+export default fetchBuffers;
 
