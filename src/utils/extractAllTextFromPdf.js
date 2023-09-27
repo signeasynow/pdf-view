dbc6f897e33b1dbd05054c4af216cbf5fc95ec1d
Alexander Leon <alexanderleon@Alexanders-MacBook-Pro.local>
2023-09-26 22:37:58 -0400

    w

--- src/utils/extractAllTextFromPdf.js
+++ src/utils/extractAllTextFromPdf.js
@@ -1,14 +1,28 @@
 export async function extractAllTextFromPDF(pdfDocument) {
-  let fullText = "";
-  
-  for(let i = 1; i <= pdfDocument.numPages; i++) {
+  const sections = [];
+
+  for (let i = 1; i <= pdfDocument.numPages; i++) {
     const page = await pdfDocument.getPage(i);
     const textContent = await page.getTextContent();
     const textItems = textContent.items;
-    const pageText = textItems.map(item => item.str).join(" ");
-    
-    fullText += pageText + " "; // Add a space between text of different pages
+
+    let paragraph = "";  // To accumulate text for a paragraph
+
+    textItems.forEach((item, index) => {
+      // Add the text to the current paragraph string.
+      paragraph += item.str;
+
+      // Heuristic: If the text ends with a character typical for ending a sentence or a paragraph,
+      // or if this is the last item, then we treat it as the end of the paragraph.
+      if (item.str.match(/[\.\?\!]\s*$/) || index === textItems.length - 1) {
+        sections.push({ page: i, paragraph: paragraph.trim() });
+        paragraph = "";
+      } else {
+        // Otherwise, add a space before the next sentence.
+        paragraph += " ";
+      }
+    });
   }
-  
-  return fullText;
+
+  return sections;
 }
