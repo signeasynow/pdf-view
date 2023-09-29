4727f79b3ca6fbe4b9cefc0143f27710c4ce6ed4
Alexander Leon <alexanderleon@Alexanders-MacBook-Pro.local>
2023-09-29 13:56:37 -0400

    w

--- src/utils/separateCitation.js
+++ src/utils/separateCitation.js
@@ -1,15 +1,31 @@
 export const separateCitation = (text) => {
-  const citationRegex = /{"citation":\s*"(.*?)"}\s*/g; // Add 'g' flag for global matching
-  let match;
+  const prefix = `{"citation": "`;
+  let idx = 0;
+  let nextIdx;
   const citations = [];
+  let mainText = '';
 
-  while ((match = citationRegex.exec(text)) !== null) {
-    const citationText = match[1].trim();
-    const splittedCitations = citationText.split('```').map(c => c.trim()).filter(Boolean);
-    citations.push(...splittedCitations);
+  while ((nextIdx = text.indexOf(prefix, idx)) !== -1) {
+    mainText += text.substring(idx, nextIdx); // Add text before citation
+    idx = nextIdx + prefix.length; // Move index to start of citation content
+
+    let endIdx = text.indexOf('"}', idx);
+    if (endIdx === -1) {
+      // Incomplete citation, take the rest of the string
+      const incompleteCitation = text.substring(idx).split('```').map(c => c.trim()).filter(Boolean);
+      citations.push(...incompleteCitation);
+      break;
+    } else {
+      // Complete citation
+      const citationText = text.substring(idx, endIdx).trim();
+      const splittedCitations = citationText.split('```').map(c => c.trim()).filter(Boolean);
+      citations.push(...splittedCitations);
+      idx = endIdx + 2; // Move index to after this complete citation
+    }
   }
 
-  const mainText = text.replace(citationRegex, '').trim();
-  
+  mainText += text.substring(idx); // Add any remaining text
+  mainText = mainText.trim();
+
   return { main: mainText, citations };
 };
