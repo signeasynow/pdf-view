31a292e2e6cd782def5b0e38a7612f7472b2012e
Alexander Leon <alexanderleon@Alexanders-MacBook-Pro.local>
2023-11-12 22:48:17 -0500

    w

--- src/App.js
+++ src/App.js
@@ -925,11 +925,13 @@ const App = () => {
 		return;
 	}
 
-	const doUpdateAnnotations = (data) => {
+	const doUpdateAnnotations = async (data) => {
+		console.log(data, 'data 3332')
 		updateAnnotation(data.source);
 		let newModifiedPayload = JSON.parse(JSON.stringify(modifiedFiles));
 		newModifiedPayload[activePageIndex] = new Date().toISOString();
 		setModifiedFiles(newModifiedPayload);
+		return await pdfProxyObj.getData();
 	}
 
 	const applyOperation = async (operation, buffer) => {
