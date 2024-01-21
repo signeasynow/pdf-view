30309ef012e0c28ea4656d9a4ca3d6748007231d
Alexander Leon <alexanderleon@Alexanders-MacBook-Pro.local>
2024-01-21 08:46:19 -0500

    w

--- src/utils/removeTextHelpersTests/findHexColor.test.mjs
+++ src/utils/removeTextHelpersTests/findHexColor.test.mjs
@@ -15,6 +15,6 @@ describe('findHexColor', () => {
   it('should work with other text at beginning', () => {
     const lines = ['/CS0 cs 0.267 0.267 0.267  scn'];
     const startIndex = 1;
-    expect(findHexColor(lines, startIndex)).to.equal('#ff0000');
+    expect(findHexColor(lines, startIndex)).to.equal('#444444');
   });
 });
